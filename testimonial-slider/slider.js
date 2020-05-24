82989e5060b9d444f19eb016b5801b2f9539bb64
nauman001 <nauman001@yahoo.com>
2020-05-24 22:39:43 +0200

    updated

--- testimonial-slider/slider.js
+++ testimonial-slider/slider.js
@@ -1,59 +1,146 @@
 const slides = document.querySelectorAll(".slide");
-const next = document.querySelector("#next");
 const prev = document.querySelector("#prev");
+const next = document.querySelector("#next");
 
 // in order to make slider auto
-// const auto = false;
-// const interval = 5000;
-// let slideInterval;    
+const auto = true;
+const intervalTime = 5000;
+let slideInterval;    
+
+// event listeners 
+
+prev.addEventListener("click", function(e) {
+  prevSlide();
+  // for auto slides change
+  if (!!auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
+
+next.addEventListener("click", function(e) {
+  nextSlide();
+  // for auto slides change
+  if (!!auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
+
+// for auto slides change
+if (!!auto) {
+  slideInterval = setInterval(nextSlide, intervalTime);
+}
 
 // functions
 
-// next slide
-const nextSlide = () => {
-  const current =  document.querySelector(".current");
-  
-  //removes current class 
-  current.classList.remove("current");
-
-  // check for next slide
-  if (!!current.nextElementSibling) {
-    // add current to next sibling element
-    current.nextElementSibling.classList.add("current");
-  } else {
-    // add current to first element
-    console.log(slides[0]);
-    slides[0].classList.add("current");
-  }
-
-  setTimeout(() => current.classList.remove("current"));
-}
-
 // previous slide
-const prevSlide = () => {
-  const current =  document.querySelector(".current");
-
-  //removes current class 
+function prevSlide() {
+  let current = document.querySelector(".current");
+  // remove current class
   current.classList.remove("current");
-  
-  // check for previous slide
+
+  // gets the previous slide and assign current class to it
   if (!!current.previousElementSibling) {
-    // add current to previous sibling element
-    console.log("prev");
     current.previousElementSibling.classList.add("current");
   } else {
-    // add current to first element
+    // add current class to the last slide
     slides[slides.length - 1].classList.add("current");
   }
-
-  setTimeout(() => current.classList.remove("current"));
 }
 
-// events listener
-next.addEventListener("click", e => {
-  nextSlide();
-});
+// next slide
+function nextSlide() {
+  let current = document.querySelector(".current");
+  // remove current class
+  current.classList.remove("current");
 
-prev.addEventListener("click", e => {
-  prevSlide();
-});
+  // gets the previous slide and assign current class to it
+  if(!!current.nextElementSibling) {
+    current.nextElementSibling.classList.add("current");
+  } else {
+    //add the current class to the first slide
+    slides[0].classList.add("current");
+  }
+}
+
+
+// ------------------------- with arrow functions ----------------------------------- //
+
+// const slides = document.querySelectorAll(".slide");
+// const next = document.querySelector("#next");
+// const prev = document.querySelector("#prev");
+
+// // in order to make slider auto
+// // const auto = false;
+// // const interval = 5000;
+// // let slideInterval;    
+
+// // functions
+
+// // next slide
+// const nextSlide = () => {
+//   const current =  document.querySelector(".current");
+  
+//   //removes current class 
+//   current.classList.remove("current");
+
+//   // check for next slide
+//   if (!!current.nextElementSibling) {
+//     // add current to next sibling element
+//     current.nextElementSibling.classList.add("current");
+//   } else {
+//     // add current to first element
+//     console.log(slides[0]);
+//     slides[0].classList.add("current");
+//   }
+
+//   setTimeout(() => current.classList.remove("current"));
+// }
+
+// // previous slide
+// const prevSlide = () => {
+//   const current =  document.querySelector(".current");
+
+//   //removes current class 
+//   current.classList.remove("current");
+  
+//   // check for previous slide
+//   if (!!current.previousElementSibling) {
+//     // add current to previous sibling element
+//     console.log("prev");
+//     current.previousElementSibling.classList.add("current");
+//   } else {
+//     // add current to first element
+//     slides[slides.length - 1].classList.add("current");
+//   }
+
+//   setTimeout(() => current.classList.remove("current"));
+// }
+
+// // events listener
+// next.addEventListener("click", e => {
+//   nextSlide();
+//   // for auto slides change
+//   if (!!auto) {
+//     clearInterval(slideInterval);
+//     slideInterval = setInterval(nextSlide, intervalTime);
+//   }
+// });
+
+// prev.addEventListener("click", e => {
+//   prevSlide();
+//   // for auto slides change
+//   if (!!auto) {
+//     clearInterval(slideInterval);
+//     slideInterval = setInterval(nextSlide, intervalTime);
+//   }
+// });
+
+// // for auto slides change
+// if (!!auto) {
+//   slideInterval = setInterval(nextSlide, intervalTime);
+// }
+
+
+
